579c4514f4e190a4a839a8a4f35176e57984b0d0
longcualonglonglong11 <nhlong18@apcs.vn>
2022-02-04 00:29:37 +0700

    Update prepare.py

--- prepare.py
+++ prepare.py
@@ -2,7 +2,7 @@ import gdown
 
 
 def download_material():
-    url = 'https://drive.google.com/uc?id=1tMTkEwKfDVh_-B0fUrn1R5Z1YwR-qXzU'
+    url = 'https://drive.google.com/uc?id=1svoqVt5AA-FarH9HApVhxY-Zfby-4fzL'
     output = 'cache/docs.txt'
     gdown.download(url, output, quiet=False)
     url = 'https://drive.google.com/uc?id=1MM33I5jI7XSOPXsuMWwq0QJBzfcUy_2S'
